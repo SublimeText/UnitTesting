8603575505a0a58e7c58bdd913ea51cc1dbfa521
Randy Lai <randy.cs.lai@gmail.com>
2014-03-04 12:06:30 -0800

    add more comments related to the requirement of __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 # since ST2 uses python 2.6, which doesn't support unittest.TestLoader.discover
 # in ST2, tests are imported as package module by unittest.TestLoader.loadTestsFromModule
-# this file is only required if your package supports ST2
+# this file is only required under 2 conditions,
+# 1) if your package supports ST2;
+# 2) if you want the tests could be reloaded in the run time of ST (eg, when you are editing the tests)
 
 import imp
 # make sure newest version of the module is loaded
