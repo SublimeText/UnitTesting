c36bd4659f8515d4ea48f269971e90026be58f3e
Randy Lai <randy.cs.lai@gmail.com>
2014-08-01 16:53:24 -0700

    escape all strings in travis.sh

--- sbin/travis.sh
+++ sbin/travis.sh
@@ -3,7 +3,7 @@
 set -e
 
 Bootstrap() {
-    STP=$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages
+    STP="$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages"
     if [ -z $(which subl) ]; then
         if [ $SUBLIME_TEXT_VERSION -eq 2 ]; then
             echo installing sublime 2
@@ -18,28 +18,28 @@ Bootstrap() {
         fi
     fi
 
-    if [ ! -d $STP ]; then
+    if [ ! -d "$STP" ]; then
         echo creating sublime package directory
-        mkdir -p $STP
+        mkdir -p "$STP"
     fi
 
-    if [ ! -d $STP/$PACKAGE ]; then
+    if [ ! -d "$STP/$PACKAGE" ]; then
         echo symlink the package to sublime package directory
-        ln -s $PWD $STP/$PACKAGE
+        ln -s "$PWD" "$STP/$PACKAGE"
     fi
 
-    if [ ! -d $STP/UnitTesting ]; then
+    if [ ! -d "$STP/UnitTesting" ]; then
         echo download latest UnitTesting release
         # for stability, you may consider a fixed version of UnitTesting, eg TAG=0.1.4
         TAG=`git ls-remote --tags https://github.com/randy3k/UnitTesting | sed 's|.*/\([^/]*$\)|\1|' | sort -r -t . -n | head -1`
-        git clone --branch $TAG https://github.com/randy3k/UnitTesting $STP/UnitTesting
+        git clone --branch $TAG https://github.com/randy3k/UnitTesting "$STP/UnitTesting"
     fi
 }
 
 RunTests() {
-    STP=$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages
-    UT=$STP/UnitTesting
-    python $UT/sbin/run.py $PACKAGE
+    STP="$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages"
+    UT="$STP/UnitTesting"
+    python "$UT/sbin/run.py" $PACKAGE
 }
 
 COMMAND=$1
