bd5a696be4f8e0423275a8acbbb8aa03f34f87e6
Randy Lai <randy.cs.lai@gmail.com>
2014-08-01 20:54:35 -0700

    travis.sh for both mac and linux

--- sbin/travis.sh
+++ sbin/travis.sh
@@ -3,18 +3,35 @@
 set -e
 
 Bootstrap() {
-    STP="$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages"
-    if [ -z $(which subl) ]; then
-        if [ $SUBLIME_TEXT_VERSION -eq 2 ]; then
-            echo installing sublime 2
-            sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
-            sudo apt-get update
-            sudo apt-get install sublime-text -y
-        elif [ $SUBLIME_TEXT_VERSION -eq 3 ]; then
-            echo installing sublime 3
-            sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
-            sudo apt-get update
-            sudo apt-get install sublime-text-installer -y
+    if [ $(uname) == 'Darwin'  ]; then
+        STP="$HOME/Library/Application Support/Sublime Text $SUBLIME_TEXT_VERSION/Packages"
+        if [ -z $(which subl) ]; then
+            brew install caskroom/cask/brew-cask
+            if [ $SUBLIME_TEXT_VERSION -eq 2 ]; then
+                echo installing sublime 2
+                brew cask install sublime-text
+                open "$HOME/Applications/Sublime Text 2.app"
+            elif [ $SUBLIME_TEXT_VERSION -eq 3 ]; then
+                echo installing sublime 3
+                brew tap caskroom/versions
+                brew cask install sublime-text3
+                open "$HOME/Applications/Sublime Text.app"
+            fi
+        fi
+    else
+        STP="$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages"
+        if [ -z $(which subl) ]; then
+            if [ $SUBLIME_TEXT_VERSION -eq 2 ]; then
+                echo installing sublime 2
+                sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
+                sudo apt-get update
+                sudo apt-get install sublime-text -y
+            elif [ $SUBLIME_TEXT_VERSION -eq 3 ]; then
+                echo installing sublime 3
+                sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
+                sudo apt-get update
+                sudo apt-get install sublime-text-installer -y
+            fi
         fi
     fi
 
@@ -37,11 +54,13 @@ Bootstrap() {
 }
 
 RunTests() {
-    if [ -z $DISPLAY ]; then
+    if  [ $(uname) != 'Darwin' ] && [ -z $DISPLAY ]; then
         export DISPLAY=:99.0
         sh -e /etc/init.d/xvfb start
+        STP="$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages"
+    elif [ $(uname) == 'Darwin' ]; then
+        STP="$HOME/Library/Application Support/Sublime Text $SUBLIME_TEXT_VERSION/Packages"
     fi
-    STP="$HOME/.config/sublime-text-$SUBLIME_TEXT_VERSION/Packages"
     UT="$STP/UnitTesting"
     python "$UT/sbin/run.py" "$PACKAGE"
 }
