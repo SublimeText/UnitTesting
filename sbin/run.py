c7436ef4971bccc6ae4480bf5235a0709add93c3
Randy Lai <randy.cs.lai@gmail.com>
2014-08-04 00:25:00 -0700

    better run script

--- sbin/run.py
+++ sbin/run.py
@@ -1,41 +1,43 @@
 import subprocess
-import tempfile
 import time, os
 import re
 import sys
+import json
 
 # cd ~/.config/sublime-text-3/Packages/UnitTesting
-# python sbin/run_scheduler.py PACKAGE
+# python sbin/run.py PACKAGE
 
-# script directory
-__dir__ = os.path.dirname(os.path.abspath(__file__))
-
+package = sys.argv[1] if len(sys.argv)>1 else "UnitTesting"
 version = int(subprocess.check_output(["subl","--version"]).decode('utf8').strip()[-4])
 
 # sublime package directory
 if sys.platform == "darwin":
-    sublime_package = os.path.expanduser("~/Library/Application Support/Sublime Text %d/Packages" % version)
+    packages_path = os.path.expanduser("~/Library/Application Support/Sublime Text %d/Packages" % version)
 elif "linux" in sys.platform:
-    sublime_package = os.path.expanduser("~/.config/sublime-text-%d/Packages" % version)
+    packages_path = os.path.expanduser("~/.config/sublime-text-%d/Packages" % version)
 
-sys.path.append(os.path.join(sublime_package, "UnitTesting"))
-from jsonio import JsonIO
-
-package = sys.argv[1] if len(sys.argv)>1 else "UnitTesting"
-
-outdir = os.path.join(sublime_package, "User", "UnitTesting", "tests_output")
+outdir = os.path.join(packages_path, "User", "UnitTesting", "tests_output")
 outfile = os.path.join(outdir, package)
+
 # remove output
 if os.path.exists(outfile): os.unlink(outfile)
 
 # add schedule
-jpath = os.path.join(sublime_package, "User", "UnitTesting", "schedule.json")
-j = JsonIO(jpath)
-schedule = j.load()
+jpath_dir = os.path.join(packages_path, "User", "UnitTesting")
+jpath = os.path.join(jpath_dir, "schedule.json")
+if not os.path.isdir(jpath_dir):
+    os.makedirs(jpath_dir)
+try:
+    with open(jpath, 'r') as f:
+        schedule = json.load(f)
+except:
+    schedule = []
 if not any([s['package']==package for s in schedule]):
     schedule.append({'package': package})
-j.save(schedule)
+with open(jpath, 'w') as f:
+    f.write(json.dumps(schedule, ensure_ascii=False, indent=True))
 
+# launch scheduler
 tasks = subprocess.check_output(['ps', 'xw']).decode('utf8')
 sublime_is_running = "Sublime" in tasks or "sublime_text" in tasks
 
