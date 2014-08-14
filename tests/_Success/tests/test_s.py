81bdfc1bd22c2eb432ef85f55811ad9f4cf4f6e0
Randy Lai <randy.cs.lai@gmail.com>
2014-08-13 17:20:40 -0700

    prevent closing wrong tab

--- tests/_Success/tests/test_s.py
+++ tests/_Success/tests/test_s.py
@@ -11,6 +11,7 @@ class TestHelloWorld(TestCase):
     def tearDown(self):
         if self.view:
             self.view.set_scratch(True)
+            self.view.window().focus_view(self.view)
             self.view.window().run_command("close_file")
 
     # since ST2 doesn't support unittest.skip, we have to do primitive skipping
--- tests/test.py
+++ tests/test.py
@@ -13,6 +13,7 @@ class TestUnitTesting(TestCase):
     def tearDown(self):
         if self.view:
             self.view.set_scratch(True)
+            self.view.window().focus_view(self.view)
             self.view.window().run_command("close_file")
 
     def test_success(self):
