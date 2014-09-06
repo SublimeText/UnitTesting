54fd779d2250191514b90d535758577d68b128c9
Randy Lai <randy.cs.lai@gmail.com>
2014-09-05 23:20:36 -0700

    Update to build 3065

--- sbin/appveyor.ps1
+++ sbin/appveyor.ps1
@@ -3,9 +3,9 @@ param([string]$command)
 Function Bootstrap {
     if ( ${env:SUBLIME_TEXT_VERSION} -eq "3" ){
         write-output "installing sublime text 3"
-        start-filedownload "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059%20x64.zip"
-        write-output "installing Sublime%20Text%20Build%203059%20x64.zip"
-        7z.exe x "Sublime%20Text%20Build%203059%20x64.zip" -o"C:\st" > $null
+        start-filedownload "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065%20x64.zip"
+        write-output "installing Sublime%20Text%20Build%203065%20x64.zip"
+        7z.exe x "Sublime%20Text%20Build%203065%20x64.zip" -o"C:\st" > $null
     }elseif ( ${env:SUBLIME_TEXT_VERSION} -eq "2" ){
         write-output "installing sublime text 2"
         start-filedownload "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.zip"
