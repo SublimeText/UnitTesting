f58a7ec406f2faea69e0d01031287acfcf1614ee
Oscar Campos <oscar@exads.com>
2014-10-07 18:06:11 +0100

    fixed PEP8 violations

--- unittesting/runner.py
+++ unittesting/runner.py
@@ -1,10 +1,39 @@
-# from https://bitbucket.org/klorenz/sublimepluginunittestharness
+# This file is based on the previous work from Kay-Uwe Lorenz in the plugin
+# sublimepluginunittestharness with the following License
+#
+# Copyright (c) 2013, Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#     Redistributions in binary form must reproduce the above copyright notice,
+#       this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
+import time
 from unittest import runner
 
-import sys, time, os, sublime
+import sublime
+
 
 class DeferringTextTestRunner(runner.TextTestRunner):
+
     r'''deferred test runner.
 
     This test runner runs tests in deferred slices.  It gives
@@ -73,12 +102,12 @@ class DeferringTextTestRunner(runner.TextTestRunner):
             result = self.condition()
 
             if not result:
-                assert (time.time() - self.condition_start_time) < 10, "Timeout, waited longer than 10s till condition true"
+                assert (time.time() - self.condition_start_time) < 10, \
+                    "Timeout, waited longer than 10s till condition true"
                 sublime.set_timeout(_wait_condition, 10)
 
             else:
                 sublime.set_timeout(_continue_testing, 10)
-
 
         def _continue_testing():
             try:
