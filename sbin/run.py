acc0bc156dd1e17b0a1c8c9a61906907fa00556c
Randy Lai <randy.cs.lai@gmail.com>
2016-03-26 22:39:38 -0400

    fix pep8

--- sbin/run.py
+++ sbin/run.py
@@ -1,5 +1,6 @@
 import subprocess
-import time, os
+import time
+import os
 import re
 import sys
 import json
@@ -11,20 +12,19 @@ import optparse
 # cd ~/.config/sublime-text-3/Packages/UnitTesting
 # python sbin/run.py PACKAGE
 
-import optparse
-
 parser = optparse.OptionParser()
 parser.add_option('--syntax-test', action="store_true", default=False)
 options, remainder = parser.parse_args()
 
 syntax_test = options.syntax_test
-package = remainder[0] if len(remainder)>0 else "UnitTesting"
+package = remainder[0] if len(remainder) > 0 else "UnitTesting"
 
-version = int(subprocess.check_output(["subl","--version"]).decode('utf8').strip()[-4])
+version = int(subprocess.check_output(["subl", "--version"]).decode('utf8').strip()[-4])
 
 # sublime package directory
 if sys.platform == "darwin":
-    packages_path = os.path.expanduser("~/Library/Application Support/Sublime Text %d/Packages" % version)
+    packages_path = os.path.expanduser(
+        "~/Library/Application Support/Sublime Text %d/Packages" % version)
 elif "linux" in sys.platform:
     packages_path = os.path.expanduser("~/.config/sublime-text-%d/Packages" % version)
 
@@ -32,7 +32,8 @@ outdir = os.path.join(packages_path, "User", "UnitTesting", "tests_output")
 outfile = os.path.join(outdir, package)
 
 # remove output
-if os.path.exists(outfile): os.unlink(outfile)
+if os.path.exists(outfile):
+    os.unlink(outfile)
 
 # add schedule
 jpath_dir = os.path.join(packages_path, "User", "UnitTesting")
@@ -44,7 +45,7 @@ try:
         schedule = json.load(f)
 except:
     schedule = []
-if not any([s['package']==package for s in schedule]):
+if not any([s['package'] == package for s in schedule]):
     schedule.append({'package': package, 'syntax_test': syntax_test})
 with open(jpath, 'w') as f:
     f.write(json.dumps(schedule, ensure_ascii=False, indent=True))
@@ -83,7 +84,7 @@ with open(outfile, 'r') as f:
         elif not result:
             f.seek(where)
         time.sleep(0.2)
-success = m.group(0)=="OK"
+success = m.group(0) == "OK"
 
 if not success:
     sys.exit(1)
