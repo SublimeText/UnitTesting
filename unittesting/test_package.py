29a5140b506b807d3cefcaf36c19518c6c5ad582
Randy Lai <randy.cs.lai@gmail.com>
2016-07-24 04:45:09 -0400

    set default omit

--- unittesting/test_package.py
+++ unittesting/test_package.py
@@ -48,15 +48,18 @@ class UnitTestingCommand(sublime_plugin.ApplicationCommand, UnitTestingMixin):
             data_file = os.path.join(package_path, ".coverage")
             config_file = os.path.join(package_path, ".coveragerc")
             include = "{}/*".format(package_path)
+            omit = "{}/{}/*".format(package_path, settings["tests_dir"])
             if os.path.exists(config_file):
                 with open(config_file, "r") as f:
                     txt = f.read()
                     if re.search("^include", txt, re.M):
                         include = None
+                    if re.search("^omit", txt, re.M):
+                        omit = None
             else:
                 config_file = None
             cov = coverage.Coverage(
-                data_file=data_file, config_file=config_file, include=include)
+                data_file=data_file, config_file=config_file, include=include, omit=omit)
             cov.start()
         else:
             cov = None
