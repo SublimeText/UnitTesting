89716f60f80c04ada4045212217963f0a76362c6
Randy Lai <randy.cs.lai@gmail.com>
2016-11-03 15:20:03 -0400

    allow setUpClass and tearDownClass in deferred testing

--- unittesting/core/st3/suite.py
+++ unittesting/core/st3/suite.py
@@ -1,4 +1,5 @@
-from unittest.suite import TestSuite, _isnotsuite
+from unittest.suite import TestSuite, _isnotsuite, _call_if_exists, _DebugResult
+from unittest import util
 
 
 class DeferrableTestSuite(TestSuite):
@@ -13,11 +14,17 @@ class DeferrableTestSuite(TestSuite):
                 break
 
             if _isnotsuite(test):
-                self._tearDownPreviousClass(test, result)
+                deferred = self._tearDownPreviousClass(test, result)
+                if deferred is not None and hasattr(deferred, '__iter__'):
+                    for x in deferred:
+                        yield x
                 yield
                 self._handleModuleFixture(test, result)
                 yield
-                self._handleClassSetUp(test, result)
+                deferred = self._handleClassSetUp(test, result)
+                if deferred is not None and hasattr(deferred, '__iter__'):
+                    for x in deferred:
+                        yield x
                 yield
                 result._previousTestClass = test.__class__
 
@@ -38,8 +45,76 @@ class DeferrableTestSuite(TestSuite):
             yield
 
         if topLevel:
-            self._tearDownPreviousClass(None, result)
+            deferred = self._tearDownPreviousClass(None, result)
+            if deferred is not None and hasattr(deferred, '__iter__'):
+                for x in deferred:
+                    yield x
+            yield
             yield
             self._handleModuleTearDown(result)
             yield
             result._testRunEntered = False
+
+    def _handleClassSetUp(self, test, result):
+        previousClass = getattr(result, '_previousTestClass', None)
+        currentClass = test.__class__
+        if currentClass == previousClass:
+            return
+        if result._moduleSetUpFailed:
+            return
+        if getattr(currentClass, "__unittest_skip__", False):
+            return
+
+        try:
+            currentClass._classSetupFailed = False
+        except TypeError:
+            # test may actually be a function
+            # so its class will be a builtin-type
+            pass
+
+        setUpClass = getattr(currentClass, 'setUpClass', None)
+        if setUpClass is not None:
+            _call_if_exists(result, '_setupStdout')
+            try:
+                deferred = setUpClass()
+                if deferred is not None and hasattr(deferred, '__iter__'):
+                    for x in deferred:
+                        yield x
+            except Exception as e:
+                if isinstance(result, _DebugResult):
+                    raise
+                currentClass._classSetupFailed = True
+                className = util.strclass(currentClass)
+                errorName = 'setUpClass (%s)' % className
+                self._addClassOrModuleLevelException(result, e, errorName)
+            finally:
+                _call_if_exists(result, '_restoreStdout')
+
+    def _tearDownPreviousClass(self, test, result):
+        previousClass = getattr(result, '_previousTestClass', None)
+        currentClass = test.__class__
+        if currentClass == previousClass:
+            return
+        if getattr(previousClass, '_classSetupFailed', False):
+            return
+        if getattr(result, '_moduleSetUpFailed', False):
+            return
+        if getattr(previousClass, "__unittest_skip__", False):
+            return
+
+        tearDownClass = getattr(previousClass, 'tearDownClass', None)
+        if tearDownClass is not None:
+            _call_if_exists(result, '_setupStdout')
+            try:
+                deferred = tearDownClass()
+                if deferred is not None and hasattr(deferred, '__iter__'):
+                    for x in deferred:
+                        yield x
+            except Exception as e:
+                if isinstance(result, _DebugResult):
+                    raise
+                className = util.strclass(previousClass)
+                errorName = 'tearDownClass (%s)' % className
+                self._addClassOrModuleLevelException(result, e, errorName)
+            finally:
+                _call_if_exists(result, '_restoreStdout')
