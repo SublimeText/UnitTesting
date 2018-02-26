58b6dd3615e4f6b35d01336bd8b9ffc5e8292ae9
Randy Lai <randy.cs.lai@gmail.com>
2018-02-26 13:13:44 -0500

    factor out stdio_splitter

--- unittesting/test_package.py
+++ unittesting/test_package.py
@@ -7,7 +7,7 @@ from unittest import TextTestRunner, TestSuite
 from .core import TestLoader, DeferringTextTestRunner, DeferrableTestCase
 from .mixin import UnitTestingMixin
 from .const import DONE_MESSAGE
-from .utils import ProgressBar
+from .utils import ProgressBar, StdioSplitter
 import threading
 
 version = sublime.version()
@@ -43,24 +43,9 @@ class UnitTestingCommand(sublime_plugin.ApplicationCommand, UnitTestingMixin):
             handler = logging.StreamHandler(stream)
             logging.root.addHandler(handler)
 
-            class StdOutForward(object):
-                def write(self, data):
-                    stdout.write(data)
-                    stream.write(data)
-                def flush(self):
-                    stderr.flush()
-                    stream.flush()
+            sys.stdout = StdioSplitter(stdout, stream)
+            sys.stderr = StdioSplitter(stderr, stream)
 
-            class StdErrForward(object):
-                def write(self, data):
-                    stderr.write(data)
-                    stream.write(data)
-                def flush(self):
-                    stderr.flush()
-                    stream.flush()
-
-            sys.stdout = StdOutForward()
-            sys.stderr = StdErrForward()
         testRunner = None
         progress_bar = ProgressBar("Testing %s" % package)
         progress_bar.start()
--- unittesting/utils/__init__.py
+++ unittesting/utils/__init__.py
@@ -3,10 +3,12 @@ from .output_panel import OutputPanel
 from . import settings as UTSetting
 from .progress_bar import ProgressBar
 from .reloader import reload_package
+from .stdio_splitter import StdioSplitter
 
 __all__ = [
     "JsonFile",
     "OutputPanel",
+    "StdioSplitter",
     "UTSetting",
     "ProgressBar",
     "reload_package"
