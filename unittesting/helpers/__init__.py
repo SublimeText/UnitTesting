79d251341afb4198daaa763c3d50a9d7078ec3d1
Thomas Smith <thom@Thomass-iMac.lan>
2018-04-10 21:40:44 -0400

    Added spaces for flake8.

--- unittesting/helpers/__init__.py
+++ unittesting/helpers/__init__.py
@@ -1,2 +1,2 @@
-from .temp_directory_test_case import TempDirectoryTestCase # noqa: F401
-from .view_test_case import ViewTestCase  # noqa: F401
+from .temp_directory_test_case import TempDirectoryTestCase  # noqa: F401
+from .view_test_case import ViewTestCase   # noqa: F401
