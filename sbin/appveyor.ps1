8825ddfd0b5aa05d4dbd62729ecb463dfc9834ea
guillermooo <guillermo.lopez@outlook.com>
2018-05-27 22:59:43 +0200

    refactor appveyor.ps1 and add utils.ps1
    - encapsulate code in functions
    - import utils.ps1 in appveyor.ps1 and refactor appveyor.ps1 to
      use new functions

--- sbin/appveyor.ps1
+++ sbin/appveyor.ps1
@@ -6,22 +6,27 @@ param(
     [switch] $coverage
 )
 
+. $PSScriptRoot\utils.ps1
 $STP = "C:\st\Data\Packages"
+$script:PackageName = $env:PACKAGE
 
 function Bootstrap {
     [CmdletBinding()]
     param(
         [switch] $with_color_scheme_unit
     )
+    
+    ensureCreateDirectory $STP
 
-    new-item -itemtype directory "$STP\${env:PACKAGE}" -force >$null
-
-    if (${env:PACKAGE} -eq "__all__"){
-        write-verbose "copy all subfolders to sublime package directory"
-        copy * -recurse -force "$STP"
+    if ($PackageName -eq "__all__"){
+        logVerbose "create package directory at $STP\$env:PACKAGE"
+        ensureCreateDirectory "$STP\$PackageName"
+        logVerbose "copy all subfolders to sublime package directory"
+        # TODO: create junctions for all packages.
+        ensureCopyDirectoryContents . "$STP"
     } else {
-        write-verbose "copy the package to sublime text Packages directory"
-        copy * -recurse -force "$STP\${env:PACKAGE}"
+        logVerbose "create directory junction to package at $STP\$PackageName"
+        ensureCreateDirectoryJunction "$STP\$env:PACKAGE" .
     }
 
     git config --global advice.detachedHead false
@@ -30,46 +35,25 @@ function Bootstrap {
     if (!(test-path -path "$UT_PATH")){
 
         $UT_URL = "https://github.com/randy3k/UnitTesting"
+        $UNITTESTING_TAG = getLatestUnitTestingBuildTag $env:UNITTESTING_TAG $env:SUBLIME_TEXT_VERSION $UT_URL
 
-        if ( ${env:UNITTESTING_TAG} -eq $null){
-            if (${env:SUBLIME_TEXT_VERSION} -eq 2) {
-                $UNITTESTING_TAG = "0.10.6"
-            } elseif (${env:SUBLIME_TEXT_VERSION} -eq 3) {
-                # the latest tag
-                $UNITTESTING_TAG = git ls-remote --tags $UT_URL | %{$_ -replace ".*/(.*)$", '$1'} `
-                        | where-object {$_ -notmatch "\^"} |%{[System.Version]$_} `
-                        | sort | select-object -last 1 | %{ "$_" }
-            }
-        } else {
-            $UNITTESTING_TAG = ${env:UNITTESTING_TAG}
-        }
-
-        write-verbose "download UnitTesting tag: $UNITTESTING_TAG"
+        logVerbose "download UnitTesting tag: $UNITTESTING_TAG"
         git clone --quiet --depth 1 --branch=$UNITTESTING_TAG $UT_URL "$UT_PATH" 2>$null
-        git -C "$UT_PATH" rev-parse HEAD | write-verbose
-        write-verbose ""
+        git -C "$UT_PATH" rev-parse HEAD | logVerbose
+        logVerbose ""
     }
 
     $COV_PATH = "$STP\coverage"
-    if ((${env:SUBLIME_TEXT_VERSION} -eq 3) -and (!(test-path -path "$COV_PATH"))){
+    if (($env:SUBLIME_TEXT_VERSION -eq 3) -and (!(test-path -path "$COV_PATH"))){
 
         $COV_URL = "https://github.com/codexns/sublime-coverage"
-
-        if ( ${env:COVERAGE_TAG} -eq $null){
-            # the latest tag
-            $COVERAGE_TAG = git ls-remote --tags $COV_URL | %{$_ -replace ".*/(.*)$", '$1'} `
-                    | where-object {$_ -notmatch "\^"} |%{[System.Version]$_} `
-                    | sort | select-object -last 1 | %{ "$_" }
-        } else {
-            $COVERAGE_TAG = ${env:COVERAGE_TAG}
-        }
-
-        write-verbose "download sublime-coverage tag: $COVERAGE_TAG"
+        $COVERAGE_TAG = getLatestCoverageTag $env:COVERAGE_TAG $COV_URL
+        
+        logVerbose "download sublime-coverage tag: $COVERAGE_TAG"
         git clone --quiet --depth 1 --branch=$COVERAGE_TAG $COV_URL "$COV_PATH" 2>$null
         git -C "$COV_PATH" rev-parse HEAD | write-verbose
-        write-verbose ""
+        logVerbose ""
     }
-
 
     & "$STP\UnitTesting\sbin\install_sublime_text.ps1" -verbose
 
@@ -83,16 +67,16 @@ function InstallPackageControl {
 
 function InstallColorSchemeUnit {
     $CSU_PATH = "$STP\ColorSchemeUnit"
-    if ((${env:SUBLIME_TEXT_VERSION} -eq 3) -and (!(test-path -path "$CSU_PATH"))){
+    if (($env:SUBLIME_TEXT_VERSION -eq 3) -and (!(test-path -path "$CSU_PATH"))){
         $CSU_URL = "https://github.com/gerardroche/sublime-color-scheme-unit"
 
-        if ( ${env:COLOR_SCHEME_UNIT_TAG} -eq $null){
+        if ( $env:COLOR_SCHEME_UNIT_TAG -eq $null){
             # the latest tag
             $COLOR_SCHEME_UNIT_TAG = git ls-remote --tags $CSU_URL | %{$_ -replace ".*/(.*)$", '$1'} `
                     | where-object {$_ -notmatch "\^"} |%{[System.Version]$_} `
                     | sort | select-object -last 1 | %{ "$_" }
         } else {
-            $COLOR_SCHEME_UNIT_TAG = ${env:COLOR_SCHEME_UNIT_TAG}
+            $COLOR_SCHEME_UNIT_TAG = $env:COLOR_SCHEME_UNIT_TAG
         }
         write-verbose "download ColorSchemeUnit tag: $COLOR_SCHEME_UNIT_TAG"
         git clone --quiet --depth 1 --branch=$COLOR_SCHEME_UNIT_TAG $CSU_URL "$CSU_PATH" 2>$null
@@ -103,16 +87,16 @@ function InstallColorSchemeUnit {
 
 function InstallKeypress {
     $KP_PATH = "$STP\Keypress"
-    if ((${env:SUBLIME_TEXT_VERSION} -eq 3) -and (!(test-path -path "$KP_PATH"))){
+    if (($env:SUBLIME_TEXT_VERSION -eq 3) -and (!(test-path -path "$KP_PATH"))){
         $KP_URL = "https://github.com/randy3k/Keypress"
 
-        if ( ${env:KEYPRESS_TAG} -eq $null){
+        if ( $env:KEYPRESS_TAG -eq $null){
             # the latest tag
             $KEYPRESS_TAG = git ls-remote --tags $KP_URL | %{$_ -replace ".*/(.*)$", '$1'} `
                     | where-object {$_ -notmatch "\^"} |%{[System.Version]$_} `
                     | sort | select-object -last 1 | %{ "$_" }
         } else {
-            $KEYPRESS_TAG = ${env:KEYPRESS_TAG}
+            $KEYPRESS_TAG = $env:KEYPRESS_TAG
         }
         write-verbose "download ColorSchemeUnit tag: $KEYPRESS_TAG"
         git clone --quiet --depth 1 --branch=$KEYPRESS_TAG $KP_URL "$KP_PATH" 2>$null
@@ -129,13 +113,13 @@ function RunTests {
     )
 
     if ( $syntax_test.IsPresent ){
-        & "$STP\UnitTesting\sbin\run_tests.ps1" "${env:PACKAGE}" -verbose -syntax_test
+        & "$STP\UnitTesting\sbin\run_tests.ps1" "$env:PACKAGE" -verbose -syntax_test
     } elseif ( $color_scheme_test.IsPresent ){
-        & "$STP\UnitTesting\sbin\run_tests.ps1" "${env:PACKAGE}" -verbose -color_scheme_test
+        & "$STP\UnitTesting\sbin\run_tests.ps1" "$env:PACKAGE" -verbose -color_scheme_test
     } elseif ( $coverage.IsPresent ) {
-        & "$STP\UnitTesting\sbin\run_tests.ps1" "${env:PACKAGE}" -verbose -coverage
+        & "$STP\UnitTesting\sbin\run_tests.ps1" "$env:PACKAGE" -verbose -coverage
     } else {
-        & "$STP\UnitTesting\sbin\run_tests.ps1" "${env:PACKAGE}" -verbose
+        & "$STP\UnitTesting\sbin\run_tests.ps1" "$env:PACKAGE" -verbose
     }
 
     stop-process -force -processname sublime_text -ea silentlycontinue
