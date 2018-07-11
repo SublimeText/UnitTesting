1728170f472451c0d3da546c886a20475090953b
guillermooo <guillermo.lopez@outlook.com>
2018-07-11 22:17:28 +0200

    refactor ci.ps1

--- sbin/ps/ci.ps1
+++ sbin/ps/ci.ps1
@@ -1,19 +1,21 @@
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory = $false, Position = 0)]
+    [Parameter(Mandatory=$false, Position=0)]
     [string]$command,
-    [Parameter(Mandatory = $false)]
+    [Parameter(Mandatory=$false)]
     [switch] $coverage
 )
 
+# Stop execution on any error. PS default is to continue on non-terminating errors.
 $ErrorActionPreference = 'stop'
 
-# TODO: Bootstrap the bootstrapper. See appveyor.ps1.
 $global:UnitTestingPowerShellScriptsDirectory = $env:TEMP
 
+# Do one-time environment initialization if needed.
 if (!$env:UNITTESTING_BOOTSTRAPPED) {
     write-output "[UnitTesting] bootstrapping environment..."
 
+    # Download scripts for basic operation.
     invoke-webrequest "https://raw.githubusercontent.com/SublimeText/UnitTesting/master/sbin/ps/ci_config.ps1" -outfile "$UnitTestingPowerShellScriptsDirectory\ci_config.ps1"
     invoke-webrequest "https://raw.githubusercontent.com/SublimeText/UnitTesting/master/sbin/ps/utils.ps1" -outfile "$UnitTestingPowerShellScriptsDirectory\utils.ps1"
     invoke-webrequest "https://raw.githubusercontent.com/SublimeText/UnitTesting/master/sbin/ps/ci.ps1" -outfile "$UnitTestingPowerShellScriptsDirectory\ci.ps1"
@@ -78,29 +80,64 @@ function RunTests {
         [switch] $coverage
     )
 
+    $script = "$UnitTestingSublimeTextPackagesDirectory\sbin\run_tests.ps1"
+
     if ($syntax_test) {
-        & "$UnitTestingSublimeTextPackagesDirectory\sbin\run_tests.ps1" "$env:PACKAGE" -verbose -syntax_test
+        & $script $env:PACKAGE -verbose -syntax_test
     } elseif ($color_scheme_test) {
-        & "$UnitTestingSublimeTextPackagesDirectory\sbin\run_tests.ps1" "$env:PACKAGE" -verbose -color_scheme_test
+        & $script $env:PACKAGE -verbose -color_scheme_test
     } elseif ($coverage) {
-        & "$UnitTestingSublimeTextPackagesDirectory\sbin\run_tests.ps1" "$env:PACKAGE" -verbose -coverage
+        & $script $env:PACKAGE -verbose -coverage
     } else {
-        & "$UnitTestingSublimeTextPackagesDirectory\sbin\run_tests.ps1" "$env:PACKAGE" -verbose
+        & $script $env:PACKAGE -verbose
     }
 
     stop-process -force -processname sublime_text -ea silentlycontinue
     start-sleep -seconds 2
 }
 
+# UnitTesting command names that the ci.ps1 script supports.
+enum CiCommand {
+    Bootstrap
+    InstallPackageControl
+    InstallColorSchemeUnit
+    InstallKeypress
+    RunTests
+    RunSyntaxTests
+    RunColorSchemeTests
+}
+
+# TODO: Remove when no longer needed.
+# Map of Legacy command names to current command names. Used to warn users of deprecated command names.
+$legacyCommandNames = @{
+    'install_package_control' = 'InstallPackageControl',
+    'install_color_scheme_unit' = 'InstallColorSchemeUnit',
+    'install_keypress' = 'InstallKeypress',
+    'run_tests' = 'RunTests',
+    'run_syntax_tests' = 'RunSyntaxTests',
+    'run_color_scheme_tests' = 'RunColorSchemeTests'
+}
+
+# TODO: Remove when no longer needed.
+if ($legacyCommandNames.ContainsKey($command)) {
+    write-warning "The command name '$command' is deprecated. Consider using '$($legacyCommandNames[$command])' instead."
+    $command = $legacyCommandNames[$command]
+}
+
+# Stop if we didn't get a valid command name.
+if (![enum]::IsDefined([CiCommand], $command)) {
+    throw ("The value of the 'Command' parameter must be one of: $([enum]::GetNames([CiCommand]) -join ", ")")
+}
+
 try{
-    switch ($command){
-        'bootstrap' { Bootstrap }
-        'install_package_control' { InstallPackageControl }
-        'install_color_scheme_unit' { InstallColorSchemeUnit }
-        'install_keypresss' { InstallKeypress }
-        'run_tests' { RunTests -coverage:$coverage }
-        'run_syntax_tests' { RunTests -syntax_test}
-        'run_color_scheme_tests' { RunTests -color_scheme_test}
+    switch ([CiCommand]$command){
+        [CiCommand]::Bootstrap { Bootstrap }
+        [CiCommand]::InstallPackageControl { InstallPackageControl }
+        [CiCommand]::InstallColorSchemeUnit { InstallColorSchemeUnit }
+        [CiCommand]::InstallKeypress { InstallKeypress }
+        [CiCommand]::RunTests { RunTests -coverage:$coverage }
+        [CiCommand]::RunSyntaxTests { RunTests -syntax_test}
+        [CiCommand]::RunColorSchemeTests { RunTests -color_scheme_test}
     }
 }catch {
     throw $_
