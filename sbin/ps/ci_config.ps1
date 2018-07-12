94fa8d89e41f94754d4b32b39df4c652ef13d975
Guillermo López-Anglada <guillermo.lopez@outlook.com>
2018-07-12 20:38:38 +0200

    add comments (#126)

--- sbin/ps/ci_config.ps1
+++ sbin/ps/ci_config.ps1
@@ -1,6 +1,28 @@
+<#
+.SYNOPSIS
+Sets up a number of global constants that can be referred from other scripts.
+
+.DESCRIPTION
+Sets up a number of global constants that can be referred from other scripts. The
+user must supply some of the values through environmental variables.
+#>
 $ErrorActionPreference = 'stop'
 
 . $PSScriptRoot\utils.ps1
+
+# Environmental variables that the user must/can set to influence values set here.
+#
+# SUBLIME_TEXT_VERSION (required)
+#   Sets the Sublime Text version to be used for testing.
+#
+# SUBLIME_TEXT_DIRECTORY
+#   Sets the directory where the Sublime Text binaries are found.
+#
+# SUBLIME_TEXT_PACKAGES_DIRECTORY
+#   Sets the Sublime Text Packages directory.
+#
+# UNITTESTING_PACKAGE_UNDER_TEST_NAME (required)
+#   Sets the name of the package being tested.
 
 # We must set constants only once.
 if (!$env:UNITTESTING_BOOTSTRAPPED) {
@@ -15,19 +37,28 @@ if (!$env:UNITTESTING_BOOTSTRAPPED) {
     # This constant means that the entire contents of the source directory must be copied to the target directory.
     makeGlobalConstant SymbolCopyAll '__all__'
 
+    # The major version of Sublime Text.
     makeGlobalConstant SublimeTextVersion (ensureValue $env:SUBLIME_TEXT_VERSION '^2|3$' -message "the environment variable SUBLIME_TEXT_VERSION must be set to '2' or '3'")
+    # True if SublimeTextVersion is 3.
     makeGlobalConstant IsSublimeTextVersion3 ($SublimeTextVersion -eq 3)
+    # True if SublimeTextVersion is 2.
     makeGlobalConstant IsSublimeTextVersion2 ($SublimeTextVersion -eq 2)
+    # The path to the Sublime Text binaries directory.
     makeGlobalConstant SublimeTextDirectory (eitherOr $env:SUBLIME_TEXT_DIRECTORY "$env:SystemDrive\st")
+    # The path to the Sublime Text executable helper.
     makeGlobalConstant SublimeTextExecutableHelperPath (join-path $SublimeTextDirectory 'subl.exe')
+    # The path to the Sublime Text executable.
     makeGlobalConstant SublimeTextExecutablePath (join-path $SublimeTextDirectory 'sublime_text.exe')
+    # The path to the Sublime Text Packages directory.
     makeGlobalConstant SublimeTextPackagesDirectory (eitherOr $env:SUBLIME_TEXT_PACKAGES_DIRECTORY (join-path $SublimeTextDirectory "Data\Packages"))
     # TODO: For compatibility; remove when not used anymore.
     $global:STP = $SublimeTextPackagesDirectory
 
+    # The name of the package being tested.
     makeGlobalConstant PackageUnderTestName (ensureValue (eitherOr $env:UNITTESTING_PACKAGE_UNDER_TEST_NAME $env:PACKAGE) -message "the environment variable UNITTESTING_PACKAGE_UNDER_TEST_NAME (or alternatively, PACKAGE) is not set")
     # TODO: For compatibility; remove when not used anymore.
     makeGlobalConstant PackageName $PackageUnderTestName
+    # The path to the Sublime Text Packages directory of the package being tested.
     makeGlobalConstant PackageUnderTestSublimeTextPackagesDirectory (join-path $SublimeTextPackagesDirectory $PackageUnderTestName)
 
     makeGlobalConstant ColorSchemeUnitRepositoryUrl "https://github.com/gerardroche/sublime-color-scheme-unit"
