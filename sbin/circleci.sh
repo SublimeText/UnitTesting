07cddc909c6f635a99d8c42c7c3ec6d7dbe4c947
Randy Lai <randy.cs.lai@gmail.com>
2019-12-03 21:31:54 -0800

    fix script bootstrap path

--- sbin/circleci.sh
+++ sbin/circleci.sh
@@ -4,11 +4,11 @@ set -e
 
 BASEDIR=`dirname $0`
 
+CISH="/tmp/ci.sh"
 if [ -f "$BASEDIR/ci.sh" ]; then
     CISH="$BASEDIR/ci.sh"
 elif [ ! -f /tmp/ci.sh ]; then
     curl -s -L https://raw.githubusercontent.com/SublimeText/UnitTesting/master/sbin/ci.sh -o /tmp/ci.sh
-    CISH="/tmp/ci.sh"
 fi
 
 REPONAME="$CIRCLE_PROJECT_REPONAME"
--- sbin/travis.sh
+++ sbin/travis.sh
@@ -10,11 +10,11 @@ fi
 
 BASEDIR=`dirname $0`
 
+CISH="/tmp/ci.sh"
 if [ -f "$BASEDIR/ci.sh" ]; then
     CISH="$BASEDIR/ci.sh"
 elif [ ! -f /tmp/ci.sh ]; then
     curl -s -L https://raw.githubusercontent.com/SublimeText/UnitTesting/master/sbin/ci.sh -o /tmp/ci.sh
-    CISH="/tmp/ci.sh"
 fi
 
 REPONAME="${TRAVIS_REPO_SLUG#*/}"
