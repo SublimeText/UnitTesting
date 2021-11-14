9b81a3f0a822f2297196ee737824e58b4266b873
Randy Lai <randy.cs.lai@gmail.com>
2021-11-13 20:58:58 -0800

    better git tag detect

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,33 +1,32 @@
-gitCloneTag() {
-    local URL="$1"
-    local TAG="$2"
-    local DEST="$3"
-    git clone --quiet --depth 1 --branch "$TAG" "$URL" "$DEST"
-}
+gitResolvePrefixToTag() {
+    local url="$1"
+    local prefix="$2"
+    local refs=$(git ls-remote --tags --heads "$url")
 
-gitFetchLatestTagFromRepository() {
-    local URL="$1"
-    local LSTAGS
-    LSTAGS=$(git ls-remote --tags "$URL")
-    local TAG=$(echo "$LSTAGS" | grep -E '/v?[0-9]+\.[0-9]+\.[0-9]+$' |
-      sed -E 's|.*/v?([^/]*)$|\1|' |
-      sort -t. -k1,1nr -k2,2nr -k3,3nr | head -n1)
-    echo "$LSTAGS" | grep -F "$TAG" | sed -E 's|.*/([^/]*)$|\1|' | grep -v '\^' | head -n1
+    local exact_match=$(echo "$refs" | sed -E -n "s%.*refs/(heads|tags)/($prefix)\$%\2%p" | head -n1)
+    if [ -n "$exact_match" ]; then
+        echo "$exact_match"
+        return 0
+    fi
+
+    if [ -z "$prefix" ]; then
+        prefix="v?"
+    fi
+
+    local matched_tags=$(echo "$refs" | sed -E -n "s%.*refs/tags/($prefix[0-9]+\.[0-9]+\.[0-9]+)\$%\1%p" | grep -v '\^{}$')
+
+    echo "$matched_tags" | sort -t. -k1,1nr -k2,2nr -k3,3nr | head -n1
 }
 
 InstallPackage() {
-    local DEST="$SUBLIME_TEXT_PACKAGES/$1"
-    local URL="$2"
-    local PreferredTag="$3"
-    if [ -z "$PreferredTag" ]; then
-        local TAG=$(gitFetchLatestTagFromRepository "$URL")
-    else
-        local TAG="$PreferredTag"
-    fi
-    echo Installing "$1" from "$URL"@"$TAG" to "$DEST"
-    if [ ! -d "$DEST" ]; then
-        mkdir -p "$DEST"
-        gitCloneTag "$URL" "$TAG" "$DEST"
-        rm -rf "$DEST/.git"
+    local dest="$SUBLIME_TEXT_PACKAGES/$1"
+    local url="$2"
+    local prefix="$3"
+    local tag=$(gitResolvePrefixToTag "$url" "$prefix")
+    echo Installing "$1" from "$url"@"$tag" to "$dest"
+    if [ ! -d "$dest" ]; then
+        mkdir -p "$dest"
+        git clone --quiet --depth 1 --branch "$tag" "$url" "$dest"
+        rm -rf "$dest/.git"
     fi
 }
