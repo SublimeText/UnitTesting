6bf4900fb42a092503667fe2e22dbfa9deb41127
Randy Lai <randy.cs.lai@gmail.com>
2021-11-15 00:42:45 -0800

    yield should be callable

--- unittesting/helpers/override_preferences_test_cast.py
+++ unittesting/helpers/override_preferences_test_cast.py
@@ -15,15 +15,16 @@ class OverridePreferencesTestCase(DeferrableTestCase):
         for settings_file_name, settings in cls.override_preferences.items():
             settings_path = os.path.join(sublime.packages_path(), "User", settings_file_name)
             new_settings_path = settings_path + ".bak"
-            s = sublime.load_settings(settings_file_name)
-            caught = [False]
-            on_change_key = settings_file_name + "catch_on_change"
-            s.add_on_change(on_change_key, lambda: caught.__setitem__(0, True))
             if os.path.exists(settings_path):
                 shutil.move(settings_path, new_settings_path)
+
+            s = sublime.load_settings(settings_file_name)
+            on_change_key = settings_file_name + "catch_on_change"
+            caught = [False]
+            s.add_on_change(on_change_key, lambda: caught.__setitem__(0, True))
             with open(settings_path, "w") as f:
                 f.write(json.dumps(settings))
-            yield caught[0]
+            yield lambda: caught[0]
             s.clear_on_change(on_change_key)
 
     @classmethod
