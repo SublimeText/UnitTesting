c39c871f752d5d77cd3478b82db1439bc5502dc5
Randy Lai <randy.cs.lai@gmail.com>
2021-11-15 01:47:29 -0800

    better catch changes

--- unittesting/helpers/override_preferences_test_cast.py
+++ unittesting/helpers/override_preferences_test_cast.py
@@ -15,16 +15,22 @@ class OverridePreferencesTestCase(DeferrableTestCase):
         for settings_file_name, settings in cls.override_preferences.items():
             settings_path = os.path.join(sublime.packages_path(), "User", settings_file_name)
             new_settings_path = settings_path + ".bak"
-            if os.path.exists(settings_path):
-                shutil.move(settings_path, new_settings_path)
 
             s = sublime.load_settings(settings_file_name)
             on_change_key = settings_file_name + "catch_on_change"
             caught = [False]
             s.add_on_change(on_change_key, lambda: caught.__setitem__(0, True))
+
+            if os.path.exists(settings_path):
+                shutil.move(settings_path, new_settings_path)
+                yield lambda: caught[0]
+                yield 500
+
+            caught = [False]
             with open(settings_path, "w") as f:
                 f.write(json.dumps(settings))
             yield lambda: caught[0]
+            yield 500
             s.clear_on_change(on_change_key)
 
     @classmethod
