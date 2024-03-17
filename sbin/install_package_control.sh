ddfd991ac70d6aa6f5735d87691ecb37ae09d78d
deathaxe <deathaxe82@googlemail.com>
2024-03-17 08:49:02 +0100

    Extend Package Control settings (#230)
    Package Control should...
    1. not mess with VCS packages
    2. not try to remove orphaned packages or libraries
    3. not try to auto-upgrade anything automatically.
    
    On Windows use encoding: utf-8 to write settings for formal reasons.
    It doesn't make a technical difference though.

--- sbin/install_package_control.sh
+++ sbin/install_package_control.sh
@@ -50,7 +50,7 @@ fi
 if [ ! -f "$STP/User/Package Control.sublime-settings" ]; then
     echo creating Package Control.sublime-settings
     # make sure Pakcage Control does not complain
-    echo '{"ignore_vcs_packages": true, "submit_usage": false }' > "$STP/User/Package Control.sublime-settings"
+    echo '{"auto_upgrade": false, "ignore_vcs_packages": true, "remove_orphaned": false, "submit_usage": false }' > "$STP/User/Package Control.sublime-settings"
 fi
 
 PCH_PATH="$STP/0_install_package_control_helper"
