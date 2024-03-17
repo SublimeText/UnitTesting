990a5b72b6d35ace158bb35c15bd9987381d79f6
deathaxe <deathaxe82@googlemail.com>
2024-03-17 10:18:49 +0100

    Fail early, if coverage is not available
    This commit moves the check for `coverage` being available to the beginning
    of `unit_testing()` method to avoid some useless work if it is not present
    and reduce indentation of most of the code.

--- unittesting/coverage.py
+++ unittesting/coverage.py
@@ -17,6 +17,11 @@ class UnitTestingCoverageCommand(UnitTestingCommand):
     fallback33 = "unit_testing33_coverage"
 
     def unit_testing(self, stream, package, settings):
+        if not coverage_loaded:
+            stream.write("Warning: coverage cannot be loaded.\n\n")
+            super().unit_testing(stream, package, settings, [])
+            return
+
         package_path = os.path.join(sublime.packages_path(), package)
         data_file = os.path.join(
             sublime.packages_path(), "User", "UnitTesting", package, "coverage")
@@ -38,46 +43,43 @@ class UnitTestingCoverageCommand(UnitTestingCommand):
         else:
             config_file = None
 
-        if coverage_loaded:
-            cov = coverage.Coverage(
-                data_file=data_file, config_file=config_file, include=include, omit=omit)
+        cov = coverage.Coverage(
+            data_file=data_file, config_file=config_file, include=include, omit=omit)
 
-            if not settings['start_coverage_after_reload']:
-                cov.start()
-            if settings["reload_package_on_testing"]:
-                self.reload_package(package, dummy=False, show_reload_progress=False)
-            if settings['start_coverage_after_reload']:
-                cov.start()
+        if not settings['start_coverage_after_reload']:
+            cov.start()
+        if settings["reload_package_on_testing"]:
+            self.reload_package(package, dummy=False, show_reload_progress=False)
+        if settings['start_coverage_after_reload']:
+            cov.start()
 
+        if settings['coverage_on_worker_thread']:
+            import threading
+            original_set_timeout_async = sublime.set_timeout_async
+
+            def set_timeout_async(callback, *args, **kwargs):
+
+                def _():
+                    sys.settrace(threading._trace_hook)
+                    callback()
+
+                original_set_timeout_async(_, *args, **kwargs)
+
+            sublime.set_timeout_async = set_timeout_async
+
+        def cleanup():
             if settings['coverage_on_worker_thread']:
-                import threading
-                original_set_timeout_async = sublime.set_timeout_async
+                sublime.set_timeout_async = original_set_timeout_async
 
-                def set_timeout_async(callback, *args, **kwargs):
+            stream.write("\n")
+            cov.stop()
+            coverage.files.RELATIVE_DIR = os.path.normcase(package_path + os.sep)
+            ignore_errors = cov.get_option("report:ignore_errors")
+            show_missing = cov.get_option("report:show_missing")
+            cov.report(file=stream, ignore_errors=ignore_errors, show_missing=show_missing)
+            if settings['generate_html_report']:
+                html_output_dir = os.path.join(package_path, 'htmlcov')
+                cov.html_report(directory=html_output_dir, ignore_errors=ignore_errors)
+            cov.save()
 
-                    def _():
-                        sys.settrace(threading._trace_hook)
-                        callback()
-
-                    original_set_timeout_async(_, *args, **kwargs)
-
-                sublime.set_timeout_async = set_timeout_async
-
-            def cleanup():
-                if settings['coverage_on_worker_thread']:
-                    sublime.set_timeout_async = original_set_timeout_async
-
-                stream.write("\n")
-                cov.stop()
-                coverage.files.RELATIVE_DIR = os.path.normcase(package_path + os.sep)
-                ignore_errors = cov.get_option("report:ignore_errors")
-                show_missing = cov.get_option("report:show_missing")
-                cov.report(file=stream, ignore_errors=ignore_errors, show_missing=show_missing)
-                if settings['generate_html_report']:
-                    html_output_dir = os.path.join(package_path, 'htmlcov')
-                    cov.html_report(directory=html_output_dir, ignore_errors=ignore_errors)
-                cov.save()
-            super().unit_testing(stream, package, settings, [cleanup])
-        else:
-            stream.write("Warning: coverage cannot be loaded.\n\n")
-            super().unit_testing(stream, package, settings, [])
+        super().unit_testing(stream, package, settings, [cleanup])
