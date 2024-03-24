384ce6887d7c94bc00891e9dcbf19457889dfe2c
deathaxe <deathaxe82@googlemail.com>
2024-03-24 12:02:52 +0100

    Improve DeferrableViewTestCase (#249)
    This commit...
    
    1. adds docstrings to briefly describe test case usage
    2. restores preferences after closing view
    3. provides basic default settings to apply to a test view via `view_settings`.
    
       Functionality, which alters content in maybe unexpected or indirect ways,
       may cause unwanted side effects.
    
    4. adds unittests for both `DeferrableViewTestCase` and `ViewTestCase`

---
+++ tests/test_deferrable_view_test_case.py
@@ -0,0 +1,48 @@
+import sublime
+from unittesting import DeferrableViewTestCase
+
+
+class TestViewTestCase(DeferrableViewTestCase):
+
+    def test_window_object(self):
+        self.assertIsInstance(self.window, sublime.Window)
+
+    def test_view_object(self):
+        self.assertIsInstance(self.view, sublime.View)
+
+    def test_view_settings(self):
+        for key, value in self.view_settings.items():
+            self.assertEqual(self.view.settings().get(key), value)
+
+    def test_clear_text_equal(self):
+        self.clearText()
+        yield 10
+        self.assertViewContentsEqual("")
+
+    def test_assert_view_content_equal(self):
+        text = "hello world\ni am here"
+        self.setText(text)
+        yield 10
+        self.assertViewContentsEqual(text)
+
+    def test_assert_row_content_equal(self):
+        self.setText("hello world\ni am here")
+        yield 10
+        self.assertRowContentsEqual(0, "hello world")
+        self.assertRowContentsEqual(1, "i am here")
+
+    def test_assert_caret_at(self):
+        self.setText("hello world")
+        yield 10
+        self.setCaretTo(0, 5)
+        yield 10
+        self.assertCaretAt(0, 5)
+
+    def test_assert_carets_at(self):
+        self.setText("hello world")
+        yield 10
+        self.setCaretTo(0, 5)
+        yield 10
+        self.addCaretAt(0, 10)
+        yield 10
+        self.assertCaretsAt(((0, 5), (0, 10)))
