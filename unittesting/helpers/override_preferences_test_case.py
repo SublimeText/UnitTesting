3d24821bd0f4ea829c29347fb72027a923c7474e
deathaxe <deathaxe82@googlemail.com>
2024-03-31 13:26:55 +0200

    Refactor OverridePreferencesTestCase

--- unittesting/helpers/override_preferences_test_case.py
+++ unittesting/helpers/override_preferences_test_case.py
@@ -1,6 +1,4 @@
 import os
-import json
-import shutil
 import sublime
 
 from ..core import DeferrableMethod
@@ -17,21 +15,25 @@ class OverridePreferencesTestCase(DeferrableTestCase):
             new_settings_path = settings_path + ".bak"
 
             s = sublime.load_settings(settings_file_name)
-            on_change_key = settings_file_name + "catch_on_change"
+            on_change_key = settings_file_name + ".catch_on_change"
             caught = [False]
             s.add_on_change(on_change_key, lambda: caught.__setitem__(0, True))
-
-            if os.path.exists(settings_path):
-                shutil.move(settings_path, new_settings_path)
+            try:
+                try:
+                    os.remove(new_settings_path)
+                except FileNotFoundError:
+                    pass
+                try:
+                    os.rename(settings_path, new_settings_path)
+                except FileNotFoundError:
+                    pass
                 yield lambda: caught[0]
-                yield 500
-
-            caught = [False]
-            with open(settings_path, "w") as f:
-                f.write(json.dumps(settings))
-            yield lambda: caught[0]
-            yield 500
-            s.clear_on_change(on_change_key)
+                caught = [False]
+                with open(settings_path, "w", encoding="utf-8") as f:
+                    f.write(sublime.encode_value(settings, True))
+                yield lambda: caught[0]
+            finally:
+                s.clear_on_change(on_change_key)
 
         deferred = super().setUpClass()
         if isinstance(deferred, DeferrableMethod):
@@ -39,16 +41,17 @@ class OverridePreferencesTestCase(DeferrableTestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for settings_file_name, settings in cls.override_preferences.items():
+        for settings_file_name in cls.override_preferences:
             settings_path = os.path.join(sublime.packages_path(), "User", settings_file_name)
             new_settings_path = settings_path + ".bak"
-            if os.path.exists(new_settings_path):
-                shutil.move(new_settings_path, settings_path)
-            else:
-                try:
-                    os.unlink(settings_path)
-                except Exception:
-                    pass
+            try:
+                os.remove(settings_path)
+            except FileNotFoundError:
+                pass
+            try:
+                os.rename(new_settings_path, settings_path)
+            except FileNotFoundError:
+                pass
 
         deferred = super().tearDownClass()
         if isinstance(deferred, DeferrableMethod):
