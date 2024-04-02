56d4d6e80b77c3900b85d7d1d511e475a349b819
deathaxe <deathaxe82@googlemail.com>
2024-04-02 20:29:19 +0200

    Drop progress bar
    1. Progress is displayed in output panel like any other build system would do.
    2. Not visible when running in CI environment.

--- unittesting/utils/__init__.py
+++ unittesting/utils/__init__.py
@@ -1,11 +1,9 @@
 from .output_panel import OutputPanel
-from .progress_bar import ProgressBar
 from .stdio_splitter import StdioSplitter
 from .reloader import reload_package
 
 __all__ = [
     "OutputPanel",
     "StdioSplitter",
-    "ProgressBar",
     "reload_package"
 ]
