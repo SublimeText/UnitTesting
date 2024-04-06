5014d30cfb2b8db4fbdd2b123b7a892b6aa5368a
deathaxe <deathaxe82@googlemail.com>
2024-04-06 11:30:30 +0200

    Fix circular import due to DeferrableTestSuite
    This commit moves (duplicates) loader module into py33 and p38 packages
    and imports it from there to avoid a circular import.

--- unittesting/core/__init__.py
+++ unittesting/core/__init__.py
@@ -4,6 +4,7 @@ if sys.version_info >= (3, 8):
     from .py38.case import DeferrableMethod
     from .py38.case import DeferrableTestCase
     from .py38.case import expectedFailure
+    from .py38.loader import DeferrableTestLoader
     from .py38.runner import AWAIT_WORKER
     from .py38.runner import DeferringTextTestRunner
     from .py38.suite import DeferrableTestSuite
@@ -11,18 +12,17 @@ else:
     from .py33.case import DeferrableMethod
     from .py33.case import DeferrableTestCase
     from .py33.case import expectedFailure
+    from .py33.loader import DeferrableTestLoader
     from .py33.runner import AWAIT_WORKER
     from .py33.runner import DeferringTextTestRunner
     from .py33.suite import DeferrableTestSuite
-
-from .loader import UnitTestingLoader as TestLoader
 
 __all__ = [
     "AWAIT_WORKER",
     "DeferrableMethod",
     "DeferrableTestCase",
+    "DeferrableTestLoader",
     "DeferrableTestSuite",
     "DeferringTextTestRunner",
     "expectedFailure",
-    "TestLoader",
 ]
